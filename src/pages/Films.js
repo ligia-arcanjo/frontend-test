a9bacad6350eb6ed16f7cdfb32b9a822b19a7261
Ligia <ligiaarcanjo84@gmail.com>
2022-10-12 20:30:04 -0300

    feat: render films with filters

--- src/pages/Films.js
+++ src/pages/Films.js
@@ -1,24 +1,47 @@
 import React, { useContext } from 'react';
 import FilmsContext from '../context/FilmsContext';
 import CardFilm from '../components/CardFilm';
+import FilterInput from '../components/FilterInput';
 
 function Films() {
-  const { filmList } = useContext(FilmsContext);
+  const {
+    filmList, nameFilter, directorFilter, orderDateFilter,
+  } = useContext(FilmsContext);
+  let allFilms = [...filmList];
+
+  const renderFilms = () => {
+    if (orderDateFilter) {
+      allFilms.sort((a, b) => Number(b.release_date) - Number(a.release_date));
+    }
+    if (directorFilter) {
+      allFilms = allFilms.filter(
+        (film) => film.director.toLowerCase().includes(directorFilter.toLowerCase()),
+      );
+    }
+    if (nameFilter) {
+      allFilms = allFilms.filter(
+        (film) => film.title.toLowerCase().includes(nameFilter.toLowerCase()),
+      );
+    }
+
+    return allFilms.map((film) => (
+      <CardFilm
+        key={film.id}
+        title={film.title}
+        banner={film.movie_banner}
+        description={film.description}
+      />
+    ));
+  };
 
   return (
     <>
       <div>Films</div>
 
+      <FilterInput />
+
       {
-        filmList.length > 0
-          && filmList.map((film) => (
-            <CardFilm
-              key={film.id}
-              title={film.title}
-              banner={film.movie_banner}
-              description={film.description}
-            />
-          ))
+        renderFilms()
       }
     </>
   );
