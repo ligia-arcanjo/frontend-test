b1ff1d714ba149449a85d7c264967030e233c462
Ligia <ligiaarcanjo84@gmail.com>
2022-10-12 12:24:06 -0300

    feat: implemented function sets request data in context

--- src/context/FilmsProvider.js
+++ src/context/FilmsProvider.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import FilmsContext from './FilmsContext';
+import fetchFilms from '../services/apiGhibli';
 
 function FilmsProvider({ children }) {
+  const [filmList, setFilmList] = useState([]);
+
+  useEffect(() => {
+    fetchFilms().then((response) => {
+      setFilmList(response);
+    });
+  }, []);
+
+  const contextValue = useMemo(() => ({ filmList }), [filmList]);
+
   return (
-    <FilmsContext.Provider value={}>
-        {children}
+    <FilmsContext.Provider value={contextValue}>
+      {children}
     </FilmsContext.Provider>
   );
 }
