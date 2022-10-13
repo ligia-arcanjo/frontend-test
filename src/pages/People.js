4cee1ddaf3e797ce7ff9346eb84e74f762cab1f6
Ligia <ligiaarcanjo84@gmail.com>
2022-10-13 00:22:47 -0300

    feat: render people page

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CardPeople from '../components/CardPeople';
+import FilterInput from '../components/FilterInput';
+import Header from '../components/Header';
+import FilmsContext from '../context/FilmsContext';
 
 function People() {
-  return (<p>People</p>);
+  const { peopleList, nameFilter } = useContext(FilmsContext);
+  let people = [...peopleList];
+
+  const renderPeople = () => {
+    if (nameFilter) {
+      people = people.filter(
+        (person) => person.name.toLowerCase().includes(nameFilter.toLowerCase()),
+      );
+    }
+
+    return people.map((person) => (
+      <CardPeople
+        key={person.id}
+        name={person.name}
+        age={person.age}
+        gender={person.gender}
+        hairColor={person.hair_color}
+        eyeColor={person.eye_color}
+      />
+    ));
+  };
+
+  return (
+    <>
+      <Header />
+
+      <FilterInput page="people" />
+
+      { renderPeople() }
+    </>
+  );
 }
 
 export default People;
