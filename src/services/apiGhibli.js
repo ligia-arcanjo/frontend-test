98bf7b00d8d79dea739355accc8752cbf5615578
Ligia <ligiaarcanjo84@gmail.com>
2022-10-13 12:31:39 -0300

    feat: implemented fetch locations

--- src/services/apiGhibli.js
+++ src/services/apiGhibli.js
@@ -25,3 +25,17 @@ export async function fetchPeople() {
     return {};
   }
 }
+
+export async function fetchLocations() {
+  const url = 'https://ghibliapi.herokuapp.com/locations';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error.message);
+    return {};
+  }
+}
