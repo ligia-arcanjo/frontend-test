1b4bc4a219b03ebb543d9be5d9c75a63a57bf3ab
Ligia <ligiaarcanjo84@gmail.com>
2022-10-13 18:28:22 -0300

    style: header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../css/Header.css';
+
 function Header() {
   return (
     <header>
-      <Link to="/">Films</Link>
-      <Link to="/people">People</Link>
-      <Link to="/locations">Locations</Link>
+      <img className="logo-ghibli" src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="logo studio ghibli" />
+
+      <div className="nav-component">
+        <Link className="nav-link" to="/">Films</Link>
+        <Link className="nav-link" to="/people">People</Link>
+        <Link className="nav-link" to="/locations">Locations</Link>
+      </div>
     </header>
   );
 }
