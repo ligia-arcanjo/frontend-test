56cdb74a7e57f92d6f9d32da4464615c14f83b4d
Ligia <ligiaarcanjo84@gmail.com>
2022-10-12 21:58:52 -0300

    fix: Povider and Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import FilmsProvider from './context/FilmsProvider';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FilmsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FilmsProvider>
   </React.StrictMode>,
 );
